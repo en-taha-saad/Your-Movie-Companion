e4b7411aa1af429f47aa90a642c9281b664172e7
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-07-06 12:51:04 +0300

    make the card composable

--- app/src/main/java/com/example/cinema_tickets/composables/HorizontalPager.kt
+++ app/src/main/java/com/example/cinema_tickets/composables/HorizontalPager.kt
@@ -1,35 +1,16 @@
 package com.example.cinema_tickets.composables
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.lerp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import coil.size.Scale
-import com.example.cinema_tickets.R
-import com.example.cinema_tickets.screens.HomeScreen
-import com.example.cinema_tickets.screens.bottomnav.BottomBarScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.rememberPagerState
-import kotlin.math.absoluteValue
-
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -41,30 +22,10 @@ fun BuildLoginSlider(sliderList: List<String>, navController: NavHostController)
         contentPadding = PaddingValues(horizontal = 75.dp),
         modifier = Modifier.height(380.dp),
     ) { page ->
-        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
-        Card(
-            colors = CardDefaults.cardColors(Color.DarkGray),
-            shape = RoundedCornerShape(25.dp),
-            modifier = Modifier
-                .graphicsLayer {
-                    lerp(
-                        start = 0.92f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                    ).also { scale ->
-                        scaleX = scale
-                        scaleY = scale
-                    }
-                }
-                .clickable { navController.navigate(BottomBarScreen.MovieDetails.route) },
-        ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current).data(sliderList[page])
-                    .crossfade(true).scale(Scale.FIT).build(),
-                placeholder = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = stringResource(R.string.app_name),
-            )
-        }
+        PagerCard(page, navController, sliderList)
     }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -74,7 +35,6 @@ fun BuildLoginSliderPreview() {
             "https://i.ibb.co/2FgBw40/design-1.png",
             "https://i.ibb.co/2FgBw40/design-1.png",
             "https://i.ibb.co/2FgBw40/design-1.png"
-        ),
-        navController = rememberNavController()
+        ), navController = rememberNavController()
     )
 }
---
+++ app/src/main/java/com/example/cinema_tickets/composables/PagerCard.kt
@@ -0,0 +1,56 @@
+package com.example.cinema_tickets.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Scale
+import com.example.cinema_tickets.R
+import com.example.cinema_tickets.screens.bottomnav.BottomBarScreen
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerScope
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import kotlin.math.absoluteValue
+
+@Composable
+@OptIn(ExperimentalPagerApi::class)
+fun PagerScope.PagerCard(
+    page: Int,
+    navController: NavHostController,
+    sliderList: List<String>
+) {
+    Card(
+        colors = CardDefaults.cardColors(Color.DarkGray),
+        shape = RoundedCornerShape(25.dp),
+        modifier = Modifier
+            .graphicsLayer {
+                val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                lerp(
+                    start = 0.92f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                ).also { scale ->
+                    scaleX = scale
+                    scaleY = scale
+                }
+            }
+            .clickable { navController.navigate(BottomBarScreen.MovieDetails.route) },
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current).data(sliderList[page])
+                .crossfade(true).scale(Scale.FIT).build(),
+            placeholder = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = stringResource(R.string.app_name),
+        )
+    }
+}
